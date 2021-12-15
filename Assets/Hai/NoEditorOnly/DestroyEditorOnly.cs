459b2e3e970c3103aa99c0f0706cc58e74bf8e24
Haï~ <hai-vr@users.noreply.github.com>
2021-12-15 06:56:32 +0100

    Add NoEditorOnly: EditorOnly objects are removed by recursion. FindGameObjectsWithTag is not used because it does not return inactive GameObjects. When onlyInsideAvatars is checked, only child objects of a VRCAvatarDescriptor will be removed. (If a VRCAvatarDescriptor is EditorOnly, it will not be removed)

---
+++ Assets/Hai/NoEditorOnly/DestroyEditorOnly.cs
@@ -0,0 +1,64 @@
+﻿// MIT License
+//
+// Copyright (c) 2021 Haï~ (@vr_hai github.com/hai-vr)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using UnityEditor;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+
+public class DestroyEditorOnly : MonoBehaviour
+{
+    public bool onlyInsideAvatars;
+
+    private void OnEnable()
+    {
+        foreach (var rootGo in gameObject.scene.GetRootGameObjects())
+        {
+            TryDestroyRecursive(rootGo, !onlyInsideAvatars);
+        }
+    }
+
+    private static void TryDestroyRecursive(GameObject subject, bool destroyPermitted)
+    {
+        // FindGameObjectsWithTag does not return inactive GameObjects
+        if (subject.tag == "EditorOnly" && destroyPermitted)
+        {
+            Destroy(subject);
+        }
+        else
+        {
+            var isSubjectAnAvatar = subject.GetComponent<VRCAvatarDescriptor>() != null;
+            foreach (Transform child in subject.transform)
+            {
+                TryDestroyRecursive(child.gameObject, destroyPermitted || isSubjectAnAvatar);
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    [MenuItem("Tools/Enable DestroyEditorOnly")]
+    public static void EnableDestroyEditorOnly()
+    {
+        var go = new GameObject("DestroyEditorOnly");
+        go.AddComponent<DestroyEditorOnly>();
+    }
+#endif
+}
